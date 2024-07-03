6b1e8c729dcc1ee98fd94f95e8c432fe9ce21969
merrymekoro <merrymekoro3m@gmail.com>
2024-07-02 22:54:09 -0500

    add admin menu and custom CSS

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -8,12 +8,17 @@ import "./assets/template/css/style.css";
 import "./assets/template/css/responsive.css";
 import "./assets/template/css/color.css";
 
+
+// ` import custom css
+import "./assets/styles/custom.css"
+
 function App() {
 
 
   return (
     <>
        <h1>Abe Garage</h1>
+       
        
     </>
   )
