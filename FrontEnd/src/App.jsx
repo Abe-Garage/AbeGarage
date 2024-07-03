60aa08f4c027c84e08b98044e40c632b5acd1d9a
merrymekoro <merrymekoro3m@gmail.com>
2024-07-03 18:08:30 -0500

    Admin DashBoard

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./markup/components/Header/Header";
 import AllOrdersPage from "./markup/pages/Admin/Orders/AllOrdersPage";
-
+import AdminDashBoard from "./markup/pages/Admin/AdminDashBoard/AdminDashBoard";
 // ` Import css from the template 
 import "./assets/template/css/bootstrap.css";
 import "./assets/template/css/style.css";
@@ -14,11 +14,13 @@ import "./assets/template/css/color.css";
 // ` import custom css
 import "./assets/styles/custom.css"
 
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/admin" element={<AdminDashBoard />} />
         <Route path="/admin/all-orders" element={<AllOrdersPage />} />
       </Routes>
     </>
