8bf59b2d84fb2ef2919630642c5ee05efed143ba
Abenezer Y <abeneyoseph@gmail.com>
2024-07-05 01:01:58 +0300

    favicon

--- FrontEnd/src/markup/pages/Main/About/About.jsx
+++ FrontEnd/src/markup/pages/Main/About/About.jsx
@@ -1,4 +1,4 @@
-import TopBannerAbout from "../../../components/Banner/TopBannerHome";
+import TopBannerAbout from "../../../components/Banner/TopBannerAbout";
 import Description from "../../../components/Description/Description";
 import About24 from "../../../components/About24/About24";
 import WhyChoosus from "../../../components/WyChoosUS/WhyChooseUs";
