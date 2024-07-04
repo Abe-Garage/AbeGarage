b26af6b3aa94667d1f5e7bea7fa6af52e7825ee6
eualx <eualx14j@gmail.com>
2024-07-04 05:51:51 +0300

    about us page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./markup/components/Header/Header";
 import AllOrdersPage from "./markup/pages/Admin/Orders/AllOrdersPage";
+import Home from './markup/pages/Home/Home';
+import About from './markup/pages/Main/About/About'
 
 // ` Import css from the template 
 import "./assets/template/css/bootstrap.css";
@@ -20,7 +22,9 @@ function App() {
     <>
       <Header />
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/admin/all-orders" element={<AllOrdersPage />} />
+        <Route path="/aboutus" element={<About />} />
       </Routes>
       <Footer/>
     </>
