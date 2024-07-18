df045349d83913408089b73ee397e62d9bbf3bff
bethelhemkidanu <betelkidanu@gmail.com>
2024-07-18 11:11:09 -0700

    search functionalty on create order

--- BackEnd/routes/order.routes.js
+++ BackEnd/routes/order.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/order.controller");
+const  searchOrder  = require("../controllers/order.controller");
 
 // Route to create a new order
 router.post("/order", orderController.createOrder);
@@ -13,5 +14,7 @@ router.get("/order/:id", orderController.getOrderById);
 
 // Route to update an order
 router.put("/order", orderController.updateOrder);
+// 
+router.get("/search-customers", orderController.searchOrder);
 
 module.exports = router;
--- FrontEnd/src/axios/axiosConfig.js
+++ FrontEnd/src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 export default axiosConfig
