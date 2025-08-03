893d70b1ff1e9a291c1b23d655870ca56d56a80d
Abenezer Y <abeneyoseph@gmail.com>
2025-08-03 14:30:34 +0300

    Update CSS imports for Vite

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -5,6 +5,11 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './Context/AuthContext.jsx'
 
+import './assets/template/css/bootstrap.css';
+import './assets/template/css/style.css';
+import './assets/template/css/responsive.css';
+import './assets/template/css/color.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
